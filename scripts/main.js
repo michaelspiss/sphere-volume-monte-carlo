c72dbb69ba66373a79f5de7ad5dc078a500d707c
michaspiss <michael.spiss@web.de>
2016-02-16 17:17:41 +0100

    Code cleanup
    new function for testing the coordinates to have a less bulky codebase

--- scripts/main.js
+++ scripts/main.js
@@ -32,6 +32,27 @@ function getRandomInt() {
     return Math.floor(Math.random() * 1000) / 1000;
 }
 
+function testCoordinates(count)
+{
+    for(var i = 0; i < count; i++)
+    {
+        var newX = getRandomInt(),
+            newY = getRandomInt(),
+            newZ = getRandomInt();
+
+        //if x^2 + y^2 + z^2 <= 1 the coordinate lies within the sphere
+        if( ( newX * newX ) + ( newY * newY ) + ( newZ * newZ ) <= 1 ) {
+            //if coordinate lies within the sphere add 1 positive test to K
+            k++;
+            //add coordinate as a positive test to the diagram, suppress redraw
+            diagram.series[1].addPoint([newX, newY, newZ], false, false, false);
+        } else {
+            //add coordinate as a negative test to the diagram, suppress redraw
+            diagram.series[0].addPoint([newX, newY, newZ], false, false, false);
+        }
+    }
+}
+
 
 function resetCalculations()
 {
@@ -59,24 +80,8 @@ function startCalculations(){
     //set the new n (old n + new n)
     n = n + newCount;
 
-    //generate n new coordinates
-    for(var i = 0; i < newCount; i++)
-    {
-        var newX = getRandomInt(),
-            newY = getRandomInt(),
-            newZ = getRandomInt();
-
-        //if x^2 + y^2 + z^2 <= 1 the coordinate lies within the sphere
-        if( ( newX * newX ) + ( newY * newY ) + ( newZ * newZ ) <= 1 ) {
-            //if coordinate lies within the sphere add 1 positive test to K
-            k++;
-            //add coordinate as a positive test to the diagram, suppress redraw
-            diagram.series[1].addPoint([newX, newY, newZ], false, false, false);
-        } else {
-            //add coordinate as a negative test to the diagram, suppress redraw
-            diagram.series[0].addPoint([newX, newY, newZ], false, false, false);
-        }
-    }
+    //generate and test n new coordinates
+    testCoordinates(newCount);
 
     //update view
     DisplayN.innerHTML = n.toString();
