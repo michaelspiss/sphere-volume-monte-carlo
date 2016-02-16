e46bf70cd537a70944bde55de24a97e4ac524225
michaspiss <michael.spiss@web.de>
2016-02-16 16:02:49 +0100

    Code cleanup, performance fixes
    adding comments to main.js, now using requestIdleCallback to schedule
    diagram redraw

--- scripts/main.js
+++ scripts/main.js
@@ -2,82 +2,115 @@
  * Copyright 2015, Michael Spiss
  * Released under the MIT license
  *
- * Date: 2015-11-27
+ * Date: 2016-02-16
  */
 
 var n = 0,
     k = 0,
-    ValueN = null,
-    ValueK = null,
-    ValueKOverN = null;
+    DisplayN = null,
+    DisplayK = null,
+    DisplayKOverN = null;
 
 $(document).ready(function(){
-    ValueN = document.getElementById('valN');
-    ValueK = document.getElementById('valK');
-    ValueKOverN = document.getElementById('valKOverN');
+    //setup
+    DisplayN = document.getElementById('valN');
+    DisplayK = document.getElementById('valK');
+    DisplayKOverN = document.getElementById('valKOverN');
+
+    //register start button handler
+    document.getElementById('startButton').onclick = function() {
+        startCalculations();
+    };
+
+    //register reset button handler
+    document.getElementById('resetButton').onclick = function() {
+        resetCalculations();
+    };
 });
 
 function getRandomInt() {
     return Math.floor(Math.random() * 1000) / 1000;
 }
 
-document.getElementById('startButton').onclick = function() {
-    startCalculations();
-};
-
-document.getElementById('resetButton').onclick = function() {
-    resetCalculations();
-};
-
 
 function resetCalculations()
 {
-    ValueN.innerHTML = '0';
-    ValueK.innerHTML = '0';
-    ValueKOverN.innerHTML = '0';
+    DisplayN.innerHTML = '0';
+    DisplayK.innerHTML = '0';
+    DisplayKOverN.innerHTML = '0';
     n = 0;
     k = 0;
-    AccessSeries.series[0].setData([], false, false, false);
-    AccessSeries.series[1].setData([], true, false, false);
+    diagram.series[0].setData([], false, false, false);
+    diagram.series[1].setData([], true, false, false);
 }
 
 function startCalculations(){
-    var maxTimes = document.getElementById('valNNew').value,
-        times = 0;
+    //get the string the user entered
+    var newCount = document.getElementById('valNNew').value;
 
-    while ( times < maxTimes ) {
-        times++;
-        n++;
+    //make sure the input is a number and if not return 0
+    newCount = !isNaN(parseInt(newCount)) ? parseInt(newCount) : 0;
+
+    //set the new n (old n + new n)
+    n = n + newCount;
+
+    //generate n new coordinates
+    for(var i = 0; i < newCount; i++)
+    {
         var newX = getRandomInt(),
             newY = getRandomInt(),
             newZ = getRandomInt();
 
+        //if x^2 + y^2 + z^2 <= 1 the coordinate lies within the sphere
         if( ( newX * newX ) + ( newY * newY ) + ( newZ * newZ ) <= 1 ) {
+            //if coordinate lies within the sphere add 1 positive test to K
             k++;
-            AccessSeries.series[1].addPoint([newX, newY, newZ], false, false, false);
+            //add coordinate as a positive test to the diagram, suppress redraw
+            diagram.series[1].addPoint([newX, newY, newZ], false, false, false);
         } else {
-            AccessSeries.series[0].addPoint([newX, newY, newZ], false, false, false);
+            //add coordinate as a negative test to the diagram, suppress redraw
+            diagram.series[0].addPoint([newX, newY, newZ], false, false, false);
         }
     }
 
-    ValueN.innerHTML = n.toString();
-    ValueK.innerHTML = k.toString();
+    //update view
+    DisplayN.innerHTML = n.toString();
+    DisplayK.innerHTML = k.toString();
+    //to prevent division by 0
     var KOverN = n == 0 ? 0 : ((k / n) * 8);
-    ValueKOverN.innerHTML = KOverN.toString();
+    DisplayKOverN.innerHTML = KOverN.toString();
 
-    //display the result before drawing
-    setTimeout(function(){
-        AccessSeries.redraw();
-    }, 10);
+
+    if ('requestIdleCallback' in window) {
+
+        //make sure the calculated value updates before trying to update the diagram (which would block the numbers)
+        function redraw(deadline)
+        {
+
+                while ((deadline.timeRemaining() > 0 || deadline.didTimeout) && tasks.length > 0)
+                    //draw all points
+                    diagram.redraw();
+
+                if (tasks.length > 0)
+                    //request an idle callback
+                    requestIdleCallback(redraw, { timeout: 2000 });
+        }
+
+    } else {
+        //if requestIdleCallback is not available give the script some time to refresh the view
+        setTimeout(function(){
+            //draw all points
+            diagram.redraw();
+        }, 10);
+    }
 
 }
 
+/*
+    diagram code (highcharts.js)
+*/
 
-
-//SCATTER PLOT
-
-
-var AccessSeries;
+var diagram;
 
 $(function () {
 
@@ -171,5 +204,5 @@ $(function () {
             }
         });
     });
-    AccessSeries = chart;
+    diagram = chart;
 });
